e388100576ad9ee3ebfe997b64069e3eca8b0a3f
ABogunets <abogunets@gmail.com>
2023-05-08 00:11:37 +0300

    ADDED: routing done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,79 +1,19 @@
-import { useState, useEffect } from "react";
-import { CardList } from "components/CardList/CardList";
-import { Container, Button } from "components/App.styled";
-import { fetchUsers } from "components/services/users-api.js";
-import { Loader } from "components/Loader/Loader";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import { Route, Routes } from "react-router-dom";
+import { lazy } from "react";
+import { SharedLayout } from "./SharedLayout/SharedLayout";
 
-// import { load, save } from "utils/storage";
+const Home = lazy(() => import("../pages/Home"));
+const Tweets = lazy(() => import("../pages/Tweets"));
 
 const App = () => {
-  const [page, setPage] = useState(1);
-  const [isLoading, setIsLoading] = useState(false);
-  const [usersSet, setUsersSet] = useState([]);
-
-  // ------LOCALstorage
-  // const isFirstRender = useRef(true);
-
-  // useEffect(() => {
-  //   const savedPage = load("currentPage");
-  //   if (savedPage > 1) {
-  //     setPage(savedPage);
-  //   }
-  // }, []);
-
-  // useEffect(() => {
-  //   if (isFirstRender.current) {
-  //     isFirstRender.current = false;
-  //     return;
-  //   }
-  //   save("page", page);
-  // }, [page]);
-  // ---
-
-  useEffect(() => {
-    setIsLoading(true);
-    const getUsers = async () => {
-      try {
-        const usersData = await fetchUsers(page);
-        setUsersSet((prevUsersSet) =>
-          page === 1 ? usersData : [...prevUsersSet, ...usersData]
-        );
-        if (usersData.length === 0 && page > 1) {
-          toast.info(`Sorry, you've reached the end of the collection!`);
-        }
-        if (usersData.length === 0 && page === 1) {
-          toast.info(`Sorry, there are no users in the collection!`);
-        }
-      } catch (err) {
-        console.log(err.message);
-        toast.error(
-          `Sorry, something went wrong with the server, please try again.`
-        );
-      } finally {
-        setIsLoading(false);
-      }
-    };
-    getUsers();
-  }, [page]);
-
-  const loadMore = () => {
-    setPage((page) => page + 1);
-  };
-
   return (
-    <Container>
-      {!isLoading && usersSet.length > 0 && <CardList usersData={usersSet} />}
-      {isLoading && <Loader />}
-      {/* <Loader /> */}
-      {!isLoading && usersSet.length > 0 && (
-        <Button onClick={loadMore} type="button">
-          Load more
-        </Button>
-      )}
-      <ToastContainer autoClose={3000} />
-    </Container>
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Home />} />
+        <Route path="tweets" element={<Tweets />} />
+        <Route path="*" element={<Home />} />
+      </Route>
+    </Routes>
   );
 };
 
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+const Home = () => {
+  return (
+    <div
+      style={{
+        fontSize: "32px",
+        color: "#4B2A99",
+      }}
+    >
+      WELCOME TO TWEETS COLLECTION!
+    </div>
+  );
+};
+
+export default Home;
